fd66d0b16a13f2710ddd51c638f78e21d37afa7c
Alexander Morales <ariel.morales@epn.edu.ec>
2025-02-03 22:34:21 -0500

    Fix: problema de seguridad
    se quitaron las credenciales para el acceso a la BD

--- Codigo/backend/database/db.js
+++ Codigo/backend/database/db.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "nova_project_db",
-  password: "1029",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 pool.on("error", (err) => {
